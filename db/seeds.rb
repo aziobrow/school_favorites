66aef810eb5726faf15ceb483fb9de8ed700212d
Aurora Ziobrowski <aziobrow@gmail.com>
2017-10-23 17:36:50 -0600

    added seeds for districts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+District.destroy_all
+
+DISTRICTS = [{"District 1" => "Anne Rowe"},
+  {"District 2" => "Rosemary Rodriguez"},
+  {"District 3" => "Mike Johnson"},
+  {"District 4" => "Rachele Espiritu"},
+  {"District 5" => "Lisa Flores"}]
+
+DISTRICTS.each do |district_info|
+  district_info.each do |k,v|
+    district = District.create(name: "#{k}", board_member: "#{v}")
+    puts "Created #{district.name}"
+  end
+end
